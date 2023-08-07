81a1b17d42c34f01eedd6741c3b1d0e1769f445a
Yurii-Br <127565054+Yurii-Br@users.noreply.github.com>
2023-08-07 21:03:51 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,52 @@
-'use strict'
+'use strict';
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const emailInp = form.querySelector('input[name="email"]');
-const txtInp = form.querySelector('textarea[name = "message"]');
+const txtInp = form.querySelector('textarea[name="message"]');
 
 const saveDataFromInp = throttle(() => {
   const formData = {
-    emaill:emailInp.value,
-    message:txtInp.value
-  }
-  localStorage.setItem('form-date' , JSON.stringify(formData) )
-},500);
+    email: emailInp.value,
+    message: txtInp.value
+  };
+  localStorage.setItem('form-date', JSON.stringify(formData));
+}, 500);
 
-
-emailInp.addEventListener('input',saveDataFromInp);
-txtInp.addEventListener('input',saveDataFromInp);
-
-window.addEventListener('DOMContentLoaded', () => {
-  const storFormData = localStorage.getItem('form-date');
-
-  if(storFormData){
-    const formData = JSON.parse(storFormData);
-     emailInp.value = formData.emaill;
-     txtInp.value = formData.message; 
-  }
-});
+emailInp.addEventListener('input', saveDataFromInp);
+txtInp.addEventListener('input', saveDataFromInp);
 
 form.addEventListener('submit', (event) => {
- event.preventDefault();
+  event.preventDefault();
 
- console.log('email:', emailInp.value);
- console.log('message:', txtInp.value);
+  if (!emailInp.value || !txtInp.value) {
+    alert('Please fill in all fields.');
+    return;
+  }
 
- emailInp.value = '';
- txtInp.value = '';
+  const formData = {
+    email: emailInp.value,
+    message: txtInp.value
+  };
 
- localStorage.removeItem('form-date');
-})
+  console.log(formData);
+
+  emailInp.value = '';
+  txtInp.value = '';
+
+  localStorage.removeItem('form-date');
+});
+
+
+function initializeForm() {
+  const storFormData = localStorage.getItem('form-date');
+
+  if (storFormData) {
+    const formData = JSON.parse(storFormData);
+    emailInp.value = formData.email;
+    txtInp.value = formData.message;
+  }
+}
+
+initializeForm();
 
